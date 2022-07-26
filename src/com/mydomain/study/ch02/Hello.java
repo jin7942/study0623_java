45ea5606923a799a38d2c09b5e732dda8cd0cac8
jin7942 <jinbuem.kim@kakao.com>
2022-07-26 17:31:16 +0900

    afd

--- src/com/mydomain/study/ch02/Hello.java
+++ src/com/mydomain/study/ch02/Hello.java
@@ -3,23 +3,25 @@ package com.mydomain.study.ch02;
 public class Hello {
 
 	public static void main(String[] args) {
+		/*		
+				String header = "아이폰 11 판매";
+				String description = "사용하지 않아 판매합니다.";
+				String name = "김진범";
+				String place = "수원시 팔달구 인계동";
+				String link = "부적절하 상품 신고하기";
 		
-		String header = "아이폰 11 판매";
-		String description = "사용하지 않아 판매합니다.";
-		String name = "김진범";
-		String place = "수원시 팔달구 인계동";
-		String link = "부적절하 상품 신고하기";
-
-		boolean cartNY = false;
-		boolean likeNY = false;
-
-		int price = 360000;
-
-		System.out.println("header: " + header);
-		System.out.println("description: " + description);
-		System.out.println("name: " + name);
-		System.out.println("place: " + place);
-		System.out.println("price: " + price);
-
+				boolean cartNY = false;
+				boolean likeNY = false;
+		
+				int price = 360000;
+		
+				System.out.println("header: " + header);
+				System.out.println("description: " + description);
+				System.out.println("name: " + name);
+				System.out.println("place: " + place);
+				System.out.println("price: " + price);
+		*/
+		
+		
 	}
 }
