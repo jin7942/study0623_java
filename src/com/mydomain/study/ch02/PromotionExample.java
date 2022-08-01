f065fe31cb6f8cc729cdb917b7172dbbbdcf9c6c
jin7942 <jinbuem.kim@kakao.com>
2022-08-01 17:29:57 +0900

    vxvx

--- src/com/mydomain/study/ch02/PromotionExample.java
+++ src/com/mydomain/study/ch02/PromotionExample.java
@@ -4,12 +4,43 @@ public class PromotionExample {
 
 	public static void main(String[] args) {
 
-		byte byteValue = 10;
-		System.out.println("byteValue: " + byteValue);
+//		byte byteValue = 10;
+//		System.out.println("byteValue: " + byteValue);
+//		
+//		int intValue = byteValue;
+//		System.out.println("intValue: " + intValue);
+//		
+//		int num1 = 10;
+//		System.out.println((short)num1);
+//
+//		long num2 = 20;
+//		System.out.println((int)num2);
+//		
+//		float num3 = 30;
+//		System.out.println((long)num3);
+//		
+//		double num4 = 40;
+//		System.out.println((float)num4);
+//		
+//		long i = 10;
+//		System.out.println((float)i);
+//
+//		int j = 20;
+//		System.out.println((long)j);
+//		
+//		int k = 30;
+//		System.out.println((short)k);
+//		
+//		short a = 40;
+//		System.out.println((byte)a);
 		
-		int intValue = byteValue;
-		System.out.println("intValue: " + intValue);
+		String str = "1245906436";
+		str = String.valueOf(str);
+		System.out.println(str);
 		
+		int num2 = 123123;
+		String str2 = Integer.toString(num2);
+		System.out.println(str2);
 	}
 
 }
