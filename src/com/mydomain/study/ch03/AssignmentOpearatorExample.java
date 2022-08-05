8c14f64febb1d74da0f13ff46751fb5ed625ff7e
jin7942 <jinbuem.kim@kakao.com>
2022-08-05 17:34:59 +0900

    asdf

---
+++ src/com/mydomain/study/ch03/AssignmentOpearatorExample.java
@@ -0,0 +1,22 @@
+package com.mydomain.study.ch03;
+
+public class AssignmentOpearatorExample {
+
+	public static void main(String[] args) {
+
+		int result = 0;
+		int result2 = 0;
+		int result3 = 0;
+
+		result += 10;
+		System.out.println("result : " + result);
+		
+		result2 -= 10;
+		System.out.println("result2 : " + result2);
+		
+		result3 *= 10;
+		System.out.println("result3 : " + result3);
+		
+	}
+
+}
